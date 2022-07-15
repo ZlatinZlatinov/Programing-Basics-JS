83ac09a5ea38e51dc67d51cc8997cdb8b6c361f6
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-15 12:15:13 +0300

    uploaded file

---
+++ First Steps in Programming/liceKvadrat.js
@@ -0,0 +1,15 @@
+function liceKvadrat() {
+let a = Number([5]);
+let area = a*a ; 
+console.log(area);
+} 
+liceKvadrat() 
+
+// // function squareArea(input){
+//     let side = Number(input[0]);
+//     let area = side * side; 
+//     console.log(area); 
+
+// } 
+//  squareArea(["5"]) 
+// za juj

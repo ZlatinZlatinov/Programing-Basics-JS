25c118e1da922ebfb31b6fd8db7dcf0200ed82c2
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-16 14:07:44 +0300

    uploaded file degrees.js
    :)

---
+++ First Steps in Programming/gradusi.js
@@ -0,0 +1,8 @@
+function gradusi (input){
+    const radian = Number(input[0]); 
+    const degrees = (radian * 180) / Math.PI; 
+    console.log(degrees); 
+// taka se dobavq komentar we
+} 
+gradusi(["3.1416"]); 
+gradusi(["6.2832"]);

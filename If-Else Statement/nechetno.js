93ef9049f741bf84f8ee7826d3cbd99a8e0baa46
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-22 12:51:37 +0300

    Add files via upload

---
+++ If-Else Statement/nechetno.js
@@ -0,0 +1,14 @@
+/*Зад.3 Задание: Да се нпише функция, която получава цяло число като аргумент и да се отпечата 
+на конзолата дали това число е четно или нечетно */ 
+function oddOrEven(input){
+    let firstNum = Number(input[0]); 
+    if (firstNum % 2 == 0){
+        console.log('even'); 
+    } else{
+        console.log('odd'); 
+    }
+
+} 
+oddOrEven(["9"]); 
+// не е пускано в жъж, оказва резултати нз дали за елсе е вярно 
+// evaaa 100/100 i moooo tikove dade i tuka

5663bf43077a508ec5287a6531383b73c999c219
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-24 23:18:08 +0300

    Add files via upload

---
+++ Switch Statement/plodove.js
@@ -0,0 +1,19 @@
+function ppap (input){
+    let plod = input[0]; 
+    switch (plod){
+        case "banana" : 
+        case "apple" : 
+        case "kiwi" : 
+        case "cherry" : 
+        case "grapes" : 
+        case "lemon" : console.log("fruit"); break;  
+        case "tomato" : 
+        case "cucumber" : 
+        case "pepper" :  
+        case "carrot" : console.log("vegetable"); break; 
+        default : console.log("unknown");
+
+    }
+} 
+ppap(["tomatoe"]); 
+// 91/100 ... 100/100 2nd tru CUCUMBER wee

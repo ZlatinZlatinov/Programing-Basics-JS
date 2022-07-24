399361a08604820988dc0c715a3a2965a81e4793
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-24 23:18:01 +0300

    Add files via upload

---
+++ Switch Statement/new.js
@@ -0,0 +1,19 @@
+function testche(input){
+    let number = Number(input[0]);  
+    let obuvki;
+    switch (number){
+        case 1: console.log("Monday"); break; 
+        case 2: console.log("Tuesday"); break; 
+        case 3: console.log("Wednesday"); break; 
+        case 4: console.log("Thursday"); break; 
+        case 5: console.log("Friday"); break; 
+        case 6: console.log("Saturday"); break; 
+        case 7: console.log("Sunday"); break; 
+        default : console.log("Error!"); 
+
+    } 
+    obuvki = "shoes"; 
+    console.log(obuvki);
+}
+testche(["8"]); 
+// Дни от седмицата със switch (ime na promenliva){}

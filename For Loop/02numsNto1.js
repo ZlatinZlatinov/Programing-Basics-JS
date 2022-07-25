089f6bd4e2ca9bbe54f07e0824f80db27d72a22a
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-25 20:20:08 +0300

    Add files via upload

---
+++ For Loop/02numsNto1.js
@@ -0,0 +1,7 @@
+function numsNtoOne(input){
+    let num = Number(input[0]); 
+    for(i = num; i >=1 ; i--){ // отпечатва числата от дадено число Н до Н-1 до 1 // 50,49,48,47.... 1
+        console.log(i);
+    }
+} 
+numsNtoOne(["50"]); // 100/100 siiiii

6d96ed194d6a3904726f9eb919a14837c30beac6
ZlatinZlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-07-25 20:21:35 +0300

    Add files via upload

---
+++ For Loop/05characters.js
@@ -0,0 +1,9 @@
+function charackters(input){
+    let text = input[0]; 
+    
+    for(let i = 0; i < text.length; i++){
+        console.log(text[i]);
+    }
+} 
+charackters(["SoftUni"]); // програма, взима даден текст и го отпечатва всяка буква на нов ред 
+// 100/100 ддз хдд

10615f8b9cdd3448c01912b35947567b9d169082
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-01 21:27:26 +0300

    Add files via upload

---
+++ While Loop Exercise/04numsdevidedby3.js
@@ -0,0 +1,10 @@
+function nums (){
+    let i = 1; 
+    while ( i <= 100){ 
+        if( i % 3 === 0){
+            console.log(i);
+        }
+        i++;  
+    } 
+} 
+nums(); // moje i taka moje i s for cikle; //100 / 100

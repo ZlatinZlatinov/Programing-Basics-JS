110d00af4f941edf90aa24aaab04bc78675c270c
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-02 13:22:45 +0300

    Add files via upload

---
+++ Nested Loops/02multiplicationTable.js
@@ -0,0 +1,9 @@
+function tablicazaUmnojenie(){
+    for(let a = 1; a < 11; a++){
+        for(let b = 1; b < 11; b++){
+            let result = a*b; 
+            console.log(`${a} * ${b} = ${result}`);
+        }
+    }
+} 
+tablicazaUmnojenie(); // 100/100 ezz
